35a77ae7ce3068ffe43133a8614c36c8e3ceae44
vivirome <vivianrochamedina@gmail.com>
2025-10-05 16:45:36 -0400

    feat: implementacion de modal al pulsar boton agregar responsable

--- src/app/private/responsables/page.tsx
+++ src/app/private/responsables/page.tsx
@@ -226,5 +226,16 @@ export default function ResponsablesPage() {
         )}
       </div>
 
-    
+    {/* Modal */}
+      {showModal && (
+        <RegisterResponsableModal
+          onClose={() => setShowModal(false)}
+          onSuccess={() => {
+            fetchResponsables(); // recargar después de registrar
+            setShowModal(false);
+          }}
+        />
+      )}
+    </div>
+  );
 }
