d991f8939bde622a3b0e485c37937cf88f6ac1eb
vivirome <vivianrochamedina@gmail.com>
2025-10-05 16:41:17 -0400

    feat: actualizacion src/app/private/responsables

--- src/app/private/responsables/layout.tsx
+++ src/app/private/responsables/layout.tsx
@@ -1,9 +1,5 @@
 'use client';
 
 export default function ResponsablesLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="p-6">
-      {children}
-    </div>
-  );
+  
 }
