d389b69a84cf59f19d566db4334f442ece486940
Juampa14 <juanpabloquiroz45@gmail.com>
2025-10-21 18:45:37 -0400

    feat: Index para algunas tabs

---
+++ src/app/private/reportes/tabs/certificados/index.tsx
@@ -0,0 +1,13 @@
+//src/app/private/reportes/tabs/certificados/index.tsx
+'use client';
+
+export default function CertificadosTab() {
+  return (
+    <div className="bg-white rounded-lg shadow p-6 text-center text-gray-600">
+      <div className="text-lg font-semibold text-black mb-1">Certificados</div>
+      <div className="text-sm">Próximamente</div>
+      <div className="text-sm">By: Juampa </div>
+      <div className="text-sm">By: Vivi</div>
+    </div>
+  );
+}
