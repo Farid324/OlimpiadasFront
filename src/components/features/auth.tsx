23f707f5f9bb51140c2f9d4b10103b24cf827a7c
Farid <geelgaga@gmail.com>
2025-10-29 18:25:31 -0400

    feat(mail): requisito deseable agregado

--- src/components/features/auth.tsx
+++ src/components/features/auth.tsx
@@ -27,11 +27,11 @@ export default function LoginForm() {
       setLoading(false);
       return;
     }
-    if (!email.endsWith('@gmail.com')) {
+    /*if (!email.endsWith('@gmail.com')) {
       setError('El correo debe pertenecer al dominio "@gmail.com".');
       setLoading(false);
       return;
-    }
+    }*/
     if (password.length < 8 || password.length > 30) {
       setError('La contraseña debe tener entre 8 y 30 caracteres.');
       setLoading(false);
