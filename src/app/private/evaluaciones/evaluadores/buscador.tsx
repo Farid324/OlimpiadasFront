d8b1c6c3a169f702ea9a2888c7f2e1183f29b49f
montmelina917 <montmelina917@gmail.com>
2025-10-31 18:32:23 -0400

    Fix: Estilos del buscador

--- src/app/private/evaluaciones/evaluadores/buscador.tsx
+++ src/app/private/evaluaciones/evaluadores/buscador.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState } from 'react';
+import { Search } from 'lucide-react';
 
 interface Props {
   onSearch: (query: string) => void;
@@ -14,13 +15,27 @@ export default function SearchBar({ onSearch }: Props) {
   };
 
   return (
-    <input
-      type="text"
-      placeholder="Buscar por nombre, CI o colegio..."
-      className="w-full rounded-lg border px-3 py-2 shadow-sm focus:ring-2 focus:ring-blue-400"
-      value={query}
-      onChange={handleChange}
-    />
+    <div className="relative w-full max-w-sm">
+      {/* Icono */}
+      <Search
+        className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"
+        size={18}
+      />
+
+      {/* Input */}
+      <input
+        type="text"
+        placeholder="Buscar por nombre, CI o colegio..."
+        value={query}
+        onChange={handleChange}
+        className="
+          w-full rounded-lg  border-gray-300 bg-white
+          pl-9 pr-3 py-2 text-sm text-gray-700
+          placeholder:text-gray-400 shadow-sm h-11 border  px-3
+            focus:outline-none focus:ring-0 focus:border-gray-400 hover:border-gray-400 transition-colors
+          "
+      />
+    </div>
   );
 }
 
